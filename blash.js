1426b51d34466b9b45ffddc572479ce80de42aea
michaelthephoenix <nkono.mic@gmail.com>
2019-05-10 15:54:59 +0300

    startup

---
+++ blash.js
@@ -0,0 +1,10 @@
+let button1 = document.getElementById('btn1');
+let txtarea = document.getElementById('textarea');
+let intro = document.getElementById('intro')
+
+
+button1.addEventListener('click', function(){
+    txtarea.innerText = "haha am getting shy again...but dont roll your eyes...";
+    intro.style.display = 'none';
+
+});
